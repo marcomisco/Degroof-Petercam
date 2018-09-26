abe3fefc7b61837802b6578f4f9d1d75cf937597
Marco Bove <bovemarco2@gmail.com>
2018-09-26 15:41:23 +0200

    retest

--- PeterProject/components/Profil/index.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import Profile from "./ProfilPage.js";
-// import EditScreenOne from "./EditScreenOne.js";
-// import EditScreenTwo from "./EditScreenTwo.js";
-import { createStackNavigator } from "react-navigation";
-export default (DrawNav = createStackNavigator({
-  Profile: { screen: Profile }
-}));
